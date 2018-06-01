bbafeb2597408eeb14d36e53d504a9b16a3040c2
Diego Salazar S <diego.salazar.se@usach.cl>
2018-06-01 11:53:49 -0400

    fix error HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,16 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        // $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
@@ -25,20 +15,20 @@ class HomeController extends Controller
     {
     	$data["tipoPasaje"] = \App\TipoPasaje::all();
 
-    	$data["localizacion"] = \App\Localizacion::all();
+    	// $data["localizacion"] = \App\Localizacion::all();
 
-    	$data["paquetes"] = \App\Paquetes::all();
-    	// $data["ciudades"] = [
-    	// 	["id"=> 1, "pais" => "Chile", "nombre" => "Santiago"],
-    	// 	["id"=> 2, "pais" => "Chile", "nombre" => "Melipilla"],
-    	// 	["id"=> 3, "pais" => "Chile", "nombre" => "Maipu"]
-    	// ];
+    	// $data["paquetes"] = \App\Paquetes::all();
+    	$data["localizacion"] = [
+    		["id"=> 1, "pais" => "Chile", "nombre" => "Santiago"],
+    		["id"=> 2, "pais" => "Chile", "nombre" => "Melipilla"],
+    		["id"=> 3, "pais" => "Chile", "nombre" => "Maipu"]
+    	];
 
-    	// $data["paquetes"] = [
-    	// 	["id" => 1, "destino" => "Chile, Santiago", "valor" => "$200.000", "class" => "active"],
-    	// 	["id" => 2, "destino" => "Chile, Rancagua", "valor" => "$250.000", "class" => ""],
-    	// 	["id" => 3, "destino" => "USA, Miami", "valor" => "$1.200.000", "class" => ""]
-    	// ];
+    	$data["paquetes"] = [
+    		["id" => 1, "destino" => "Chile, Santiago", "valor" => "$200.000", "class" => "active"],
+    		["id" => 2, "destino" => "Chile, Rancagua", "valor" => "$250.000", "class" => ""],
+    		["id" => 3, "destino" => "USA, Miami", "valor" => "$1.200.000", "class" => ""]
+    	];
 
         return view('home', compact("data"));
     }
