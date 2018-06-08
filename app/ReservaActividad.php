168ba8920bcdbf2924c68a726c1cf541f4a09d50
VBrevis <vicente.brevis@usach.cl>
2018-06-08 11:52:40 -0400

    Correccion nombre de modelo ReservaActividad

--- app/ReservaActividad.php
+++ app/ReservaActividad.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Reserva_actividades extends Model
+class Reserva_actividad extends Model
 {
     protected $fillable = [
         'fecha_reserva',
