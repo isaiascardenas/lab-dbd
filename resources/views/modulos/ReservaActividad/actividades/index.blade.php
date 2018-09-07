12dbd285c3cdd71ff734d7e29c22fc5a2b2d7a26
Isaias Cardenas <isaias.cardenas@usach.cl>
2018-09-07 11:40:46 -0300

    Renombra carpeta en ReservaAcitividades

---
+++ resources/views/modulos/ReservaActividad/actividades/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.admin')
+
+@section('content')
+	<h2>
+    <i class="fas fa-map-marker-alt"></i> Actividades
+  </h2>
+	
+	<hr>
+
+	@include('layouts.messages')
+	
+	<div class="form-group">
+		<a href="/actividades/create/" class="btn btn-primary">
+      <i class="fas fa-plus"></i> Nueva Actividad
+    </a>
+	</div>
+
+	<table class="table table-hover table-bordered table-sm datatable">
+		<thead>
+			<tr>
+				<th class="no-sort"></th>
+				<th>Descripcion</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($actividades as $actividad)
+			<tr>
+				<td>
+          <a class="btn btn-sm btn-info" href="/actividades/{{ $actividad->id }}">
+            <i class="fas fa-eye"></i>
+          </a>
+        </td>
+				<td>{{ $actividad->descripcion }}</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+@endsection
+
+@section('script')
+  <script>
+    $(document).ready(function() {
+        $('.datatable').DataTable({
+          'language': {
+            'url': 'https://cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json'
+          },
+          'columnDefs': [
+            {'targets': 'no-sort', 'orderable': false}
+          ], 
+          'order': [[1, 'asc']]
+        });
+    } );
+  </script>
+@endsection
