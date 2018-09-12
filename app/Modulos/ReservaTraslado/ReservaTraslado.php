8d5e5ff8ceb7755af350f651f50d6156b3279781
Isaias Cardenas <isaias.cardenas@usach.cl>
2018-09-12 17:54:06 -0300

    Resuelve conflictos git

--- app/Modulos/ReservaTraslado/ReservaTraslado.php
+++ app/Modulos/ReservaTraslado/ReservaTraslado.php
@@ -6,24 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class ReservaTraslado extends Model
 {
-  protected $table = 'reserva_traslados';
+    protected $table = 'reserva_traslados';
 
-  protected $fillable = [
-    'cantidad_pasajeros',
-    'fecha_reserva',
-    'descuento',
-    'costo',
-    'traslado_id',
-    'orden_compra_id'
-  ];
+    protected $fillable = [
+        'cantidad_pasajeros',
+        'fecha_reserva',
+        'descuento',
+        'costo',
+        'traslado_id',
+        'orden_compra_id'
+    ];
 
-  public function traslados()
-  {
-    $this->belongsTo(Traslado::class);
-  }
+    public function traslados()
+    {
+        $this->belongsTo(Traslado::class);
+    }
 
-  public function ordenCompra()
-  {
-    $this->belongsTo(OrdenCompra::class);
-  }
+    public function ordenCompra()
+    {
+        $this->belongsTo(OrdenCompra::class);
+    }
 }
--- app/Modulos/ReservaTraslado/Traslado.php
+++ app/Modulos/ReservaTraslado/Traslado.php
@@ -2,36 +2,36 @@
 
 namespace App\Modulos\ReservaTraslado;
 
-
 use App\ReservaVuelo\Aeropuerto;
 use Illuminate\Database\Eloquent\Model;
-use App\Modulos\ReservaHabitacion\Habitacion;
+use App\Modulos\ReservaHabitacion\Hotel;
 
 
 class Traslado extends Model
 {
-  protected $table = 'traslados';
+    protected $table = 'traslados';
 
-  protected $fillable = [
-    'tipo',
-    'fecha_inicio',
-    'fecha_termino',
-    'aeropuerto_id',
-    'hotel_id'
-  ];
+    protected $fillable = [
+        'tipo',
+        'fecha_inicio',
+        'fecha_termino',
+        'capacidad',
+        'aeropuerto_id',
+        'hotel_id'
+    ];
 
-  public function aeropuerto()
-  {
-    return $this->belongsTo(Aeropuerto::class);
-  }
+    public function aeropuerto()
+    {
+        return $this->belongsTo(Aeropuerto::class);
+    }
 
-  public function hotel()
-  {
-    return $this->belongsTo(Hotel::class);
-  }
+    public function hotel()
+    {
+        return $this->belongsTo(Hotel::class);
+    }
 
-  public function reservas()
-  {
-    return $this->hasMany(ReservaTraslado::class);
-  }
+    public function reservas()
+    {
+        return $this->hasMany(ReservaTraslado::class);
+    }
 }
