4186e1d58ea89c900d10b1535037dfe595c4f442
Isaias Cardenas <isaias.cardenas@usach.cl>
2018-09-13 13:33:07 -0300

    Agrega formato a fechas de traslado

--- app/Modulos/ReservaTraslado/ReservaTraslado.php
+++ app/Modulos/ReservaTraslado/ReservaTraslado.php
@@ -25,6 +25,16 @@ class ReservaTraslado extends Model
                 : $this->costo;
     }
 
+    public function fechaInicio($format = 'H:i d-m-Y')
+    {
+      return Carbon::parse($this->fecha_inicio)->format($format);
+    }
+
+    public function fechaTermino($format = 'H:i d-m-Y')
+    {
+      return Carbon::parse($this->fecha_termino)->format($format);
+    }
+
     public function traslado()
     {
         return $this->belongsTo(Traslado::class);
