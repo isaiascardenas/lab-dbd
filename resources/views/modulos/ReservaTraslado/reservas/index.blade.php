4a296d293a4c0b1b0eae89cd0c725ef2736e25a2
ngutierrezp <nicolas.gutierrez.p@usach.cl>
2018-09-13 05:57:15 -0300

    Se agrega vista index para traslados

--- resources/views/modulos/ReservaTraslado/reservas/index.blade.php
+++ resources/views/modulos/ReservaTraslado/reservas/index.blade.php
@@ -1,53 +1,88 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>
-        <i class="fas fa-calendar-alt"></i> Selecciona tu traslado
-    </h2>
+  <h2>
+    <i class="fas fa-bus"></i> Selecciona tu Traslado
+  </h2>
 
+  @if (count($traslados) > 0)
     <div id="list-accordion">
-        @foreach($traslados as $traslado)
-            <div class="card">
-                <a class="card-header" data-toggle="collapse" href="#traslado-{{ $loop->iteration }}">
-                    {{ $traslado->titulo }}
-                    <div class="text-right float-right">
-                        {{ $traslado->precioAdulto(TRUE) }} / adulto
-                    </div>
-                </a>
-
-                <div class="collapse" id="traslado-{{ $loop->iteration }}" data-parent="#list-accordion">
-                    <div class="card-body">
-                        <p>
-                        <b>Descripción</b>
-                        <br>
-                        {{ $traslado->descripcion }}
-                        </p>
-                        <p class="card-text">
-                        <span class="text-right font-weight-bold">
-                            Precio Adulto: {{ $traslado->precioAdulto(TRUE) }}
-                        </span >
-                        <br/>
-                        <span class="text-right font-weight-bold">
-                            Max cantidad adulto: {{ $traslado->max_adultos }}
-                        </span >
-                        <br/>
-                        <br/>
-                        <span class="text-right font-weight-bold">
-                            Precio Niño: {{ $traslado->precioNino(TRUE) }}
-                        </span >
-                        <br/>
-                        <span class="text-right font-weight-bold">
-                            Max cantidad niños: {{ $traslado->max_ninos }}
-                        </span >
-                        </p>
-                        <div class="text-right">
-                            <a class="btn btn-sm btn-info" href="/reserva-traslados/reservar/{{ $traslado->id }}">
-                                <i class="fas fa-cart-plus"></i> Agregar al carro
-                            </a>
-                        </div>
-                    </div>
+      @foreach($traslados as $traslado)
+      <div class="card">
+        <a class="card-header" data-toggle="collapse" href="#traslado-{{ $loop->iteration }}">
+          <div class="row text-left">
+            <div class="col-6">
+              <div class="row">
+                <div class="col">
+                  <span class="font-weight-bold">{{ $traslado->horaPartida() }}</span>
+                  <span class="text-muted">
+                  (Hora inicio de traslado)</span>
                 </div>
+
+                <div class="col-1">
+                  <i class="fas fa-angle-right"></i>
+                </div>
+
+                <div class="col">
+                  <span class="font-weight-bold">{{ $traslado->horaLlegada() }}</span>
+                  <span class="text-muted">
+                  (Hora llegada a destino)</span>
+                </div>
+              </div>
             </div>
-        @endforeach
+
+            <div class="col-2 text-muted">
+                {{$traslado->stringTipoTraslado()}}
+            </div>
+
+
+            <div class="col-2 text-muted">
+                
+            </div>
+
+            <div class="col-2 text-right font-weight-bold">
+                <span class="text-muted">
+                  Precio por persona</span>
+                $ {{ $traslado->precio_persona}}
+            </div>
+          </div>
+        </a>
+          
+        <div class="collapse" id="traslado-{{ $loop->iteration }}" data-parent="#list-accordion">
+          <div class="card-body">
+            <p class="card-text">
+              <b>Detalles</b>
+              <br>Hotel: {{$traslado->hotel->nombre}}</br>
+              <br>Aeropuerto: {{$traslado->aeropuerto->nombre}}, ({{$traslado->aeropuerto->codigo}})</br>
+              <br> capacidad maxima : {{$traslado->capacidad}}
+              
+            </p>
+            
+            <div class="text-right">
+              <form action="/vuelos/details/" method="post">
+                {{ csrf_field() }}
+        
+                <button type="submit" class="btn btn-info btn-sm">
+                  <i class="fas fa-check"></i>
+                  Seleccionar
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      @endforeach
     </div>
+  @else
+    <div class="alert alert-danger">
+      <b>Whoops</b>
+      <br>
+      No se han encontrado Traslados para los datos ingresados
+      <br>
+      <a href="/" class="btn btn-link">
+        <i class="fas fa-arrow-left"></i>
+        Volver
+      </a>
+    </div>
+  @endif
 @endsection
